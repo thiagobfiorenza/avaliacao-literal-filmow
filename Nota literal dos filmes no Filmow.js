43265f7f4bab1e6c1fd70fddb4925357555afd78
Thiago Bueno Fiorenza <thiagobfiorenza@gmail.com>
2016-10-19 17:17:43 -0200

    Alterando script para aceitar o formato do OpenUserJS

--- Nota literal dos filmes no Filmow.js
+++ Nota literal dos filmes no Filmow.js
@@ -9,16 +9,16 @@
 
 $('.navbar-sub.navbar-movie .span9').append('<div id="barrinhaInusitada" style="background-color: #51A351; color: #FFFFFF; display: none; font-size: 12px; font-weight: bold; height: 17px; margin-left: 358px; margin-top: -33px; padding: 11px 0; position: absolute; text-align: center; text-shadow: 2px 1px 1px #000000; width: 343px;">');
 $('.star-rating .half-star[data-value=1]').hover(function(){
-    $('#barrinhaInusitada').html('Lastimável (Um dos piores filmes da minha vida)').css('backgroundColor', '#CA0000').show();
+    $('#barrinhaInusitada').html('Lastimavel (Um dos piores filmes da minha vida)').css('backgroundColor', '#CA0000').show();
 });
 $('.star-rating .half-star[data-value=2]').hover(function(){
-    $('#barrinhaInusitada').html('Horrível (Tempo de vida que não vai voltar mais)').css('backgroundColor', '#CA0000').show();
+    $('#barrinhaInusitada').html('Horrivel (Tempo de vida que não vai voltar mais)').css('backgroundColor', '#CA0000').show();
 });
 $('.star-rating .half-star[data-value=3]').hover(function(){
-    $('#barrinhaInusitada').html('Péssimo (Troféu Framboesa)').css('backgroundColor', '#CA0000').show();
+    $('#barrinhaInusitada').html('Pessimo (Trofeu Framboesa)').css('backgroundColor', '#CA0000').show();
 });
 $('.star-rating .half-star[data-value=4]').hover(function(){
-    $('#barrinhaInusitada').html('Muito ruim (Não serve nem para Sessão da Tarde)').css('backgroundColor', '#CA0000').show();
+    $('#barrinhaInusitada').html('Muito ruim (Nao serve nem para Sessao da Tarde)').css('backgroundColor', '#CA0000').show();
 });
 $('.star-rating .half-star[data-value=5]').hover(function(){
     $('#barrinhaInusitada').html('Ruim (Falhou em alguns detalhes)').css('backgroundColor', '#CA0000').show();
@@ -30,13 +30,13 @@ $('.star-rating .half-star[data-value=7]').hover(function(){
     $('#barrinhaInusitada').html('Muito bom (Veria novamente)').css('backgroundColor', '#51A351').show();
 });
 $('.star-rating .half-star[data-value=8]').hover(function(){
-    $('#barrinhaInusitada').html('Ótimo (Merece um prêmio!)').css('backgroundColor', '#51A351').show();
+    $('#barrinhaInusitada').html('Otimo (Merece um prêmio!)').css('backgroundColor', '#51A351').show();
 });
 $('.star-rating .half-star[data-value=9]').hover(function(){
     $('#barrinhaInusitada').html('Excelente (Entrou para os Favoritos!)').css('backgroundColor', '#51A351').show();
 });
 $('.star-rating .half-star[data-value=10]').hover(function(){
-    $('#barrinhaInusitada').html('Simplesmente épico (Como não tive essa ideia!?)').css('backgroundColor', '#51A351').show();
+    $('#barrinhaInusitada').html('Simplesmente epico (Como nao tive essa ideia!?)').css('backgroundColor', '#51A351').show();
 });
 $('.star-rating .half-star').mouseout(function(){
     $('#barrinhaInusitada').html('').hide();
